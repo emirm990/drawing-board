b3d76e55824dec1e2b7654ef1f0b8dc74ff9a3b0
emirm990 <emir.mekic@live.com>
2018-06-10 22:19:33 +0200

    JavaScrip cleanup
    Cleaned up js code.

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
-    var color = "black";
+    let color = "black";
+    let box_size = 15;
+    let selectValues=[10,5];
+    let xValue = selectValues[0];
+    let yValue = selectValues[1];
+    function boxSizing(box_size,xValue){
+        $(".clickable").css("height", box_size+"px");
+        $(".clickable").css("width", box_size+"px");
+        $(".row").css("height", box_size+"px");
+        $(".row").css("min-width", xValue*box_size+"px");
+        $("#drawing_board").css("min-width",(xValue*box_size+50)+"px");
+        $("main").css("min-width",(xValue*box_size+100)+"px");
+    };
+    function gridMaker(){
+        for(let i=0;i<=yValue;i++){
+            $("#drawing_board").append("<div class='row'> </div>");
+        };
+        for (let j=0;j<=xValue;j++){
+            $(".row").append("<div class='clickable'></div>");
+        };
+        boxSizing(box_size,xValue);
+    };
+    gridMaker();
     $('#colors input').on('click', function(){
         color = $(this).val();
     });
@@ -16,45 +38,30 @@ $(document).ready(function(){
     $("#clear").on("click", function(){
         $('.clickable').css("background-color", "white");
     });
-    let box_size = 15;
-    let selectValues=[10,5];
-    let xValue = selectValues[0];
-    let yValue = selectValues[1];
     $("#box_size").on("change", function(){
         box_size = this.value.slice(0,-2);
         console.log(box_size);
-        $(".clickable").css("height", box_size+"px");
-        $(".clickable").css("width", box_size+"px");
-        $(".row").css("height", box_size+"px");
-        $(".row").css("min-width", xValue*box_size+"px");
-        $("#drawing_board").css("min-width",(xValue*box_size+50)+"px");
-        $("main").css("min-width",(xValue*box_size+100)+"px");
+        boxSizing(box_size,xValue);   
     });
     $('#grid_size').on('change', function() {
         $("#drawing_board").empty();
-        
         selectValues=this.value.split("x");
         xValue = Number(selectValues[0]);
         yValue = Number(selectValues[1]);
         console.log(box_size);
-        for(let i=0;i<=yValue;i++){
-            $("#drawing_board").append("<div class='row'> </div>");
-        };
-        for (let j=0;j<=xValue;j++){
-            $(".row").append("<div class='clickable'></div>");
-        };
-        $(".clickable").css("height", box_size+"px");
-        $(".clickable").css("width", box_size+"px");
-        $(".row").css("height", box_size+"px");
-        $(".row").css("min-width", xValue*box_size+"px");
-        $("#drawing_board").css("min-width",(xValue*box_size+50)+"px");
-        $("main").css("min-width",(xValue*box_size+100)+"px");
+        gridMaker();
+        boxSizing(box_size,xValue);
         console.log(box_size);
+        if($('#circle').is(':checked')){
+            $(".clickable").css("border-radius", "50%");
+        }else{
+            $(".clickable").css("border-radius", "0");
+        }
       });
-    $("input[val='circle']").on("click", function(){
+    $("#circle").on("click", function(){
         $(".clickable").css("border-radius", "50%");
     });
-    $("input[val='square']").on("click", function(){
+    $("#square").on("click", function(){
         $(".clickable").css("border-radius", "0");
     });
 });
